3816df317170c3d22d31589c633d0a5983545461
theshantanujoshi <theshantanujoshi@users.noreply.github.com>
2026-09-11 23:08:35 +0530

    chore: speed up signature animation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ export function Footer() {
         {/* Center: Signature */}
         <div className="flex justify-center items-center flex-1 order-1 md:order-2">
           <div className="scale-75 md:scale-90 h-[50px] flex items-center justify-center">
-            <SignatureAnimation duration={1.0} delay={0.15}>
+            <SignatureAnimation duration={0.8} delay={0.1}>
               Shantanu Joshi
             </SignatureAnimation>
           </div>
